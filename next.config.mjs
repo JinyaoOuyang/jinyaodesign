90118765497ae47c9d785b6fcf09d7397c4346b5
Jinyao Ouyang <katherineoyjy@gmail.com>
2026-02-10 16:57:42 -0500

    redesign blog page with card layout, cover images, and improved spacing and transitions

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  images: {
-    formats: ['image/avif', 'image/webp'],
-  },
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+    images: {
+        formats: ['image/avif', 'image/webp'],
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: 'cdn-images-1.medium.com',
+        }, ],
+    },
 }
 
 export default nextConfig
